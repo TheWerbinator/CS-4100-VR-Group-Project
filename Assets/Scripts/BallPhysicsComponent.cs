a3cddb42ad72e263db8aad8f650a601c8648d076
Shawn <shawnwerber@suu.edu>
2025-04-01 23:07:42 -0600

    Added basic ball-paddle interaction

--- Assets/Scripts/BallPhysicsComponent.cs
+++ Assets/Scripts/BallPhysicsComponent.cs
@@ -1,33 +1,71 @@
-using System;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class BallPhysicsComponent : MonoBehaviour
 {
   public enum BallState { Held, Serving, Waiting, Active }
-  private float _gravity = 9.89f;
-  private Vector3 _velocity = new Vector3(0, 0, 0);
-  private Rigidbody _body;
-  private BallState _ballState;
-  // Start is called before the first frame update
+
+  public BallState ballState = BallState.Held;
+  public float serveForce = 5f;
+
+  private Rigidbody rb;
+
   void Start()
   {
-    _velocity = Vector3.down * _gravity;
-    _body = GetComponent<Rigidbody>();
-    // BallState = this.BallState;
-    _ballState = BallState.Held;
+    rb = GetComponent<Rigidbody>();
+    rb.useGravity = false; // We'll enable it when serving
   }
 
-  // Update is called once per frame
   void Update()
   {
-    if (_ballState == BallState.Held || BallState.Waiting) return;
-    if (_ballState == BallState.Serve)
+    switch (ballState)
     {
-      _velocity += _gravity * Time.deltaTime;
-      _body.position += _velocity * Time.deltaTime;
-      //if The position of the ball is far enough up in the air
-      _ballState = BallState.Waiting;
-    }
+      case BallState.Held:
+        rb.velocity = Vector3.zero;
+        rb.useGravity = false;
 
+        // if (controller button is pushed)
+        // {
+        //   ballState = BallState.Serving;
+        // }
+        break;
+
+      case BallState.Serving:
+        ServeBall();
+        break;
+
+      case BallState.Waiting:
+        // maybe check for user interaction here
+        break;
+
+      case BallState.Active:
+        // physics engine handles this
+        break;
+    }
+  }
+
+  void OnCollisionEnter(Collision collision)
+  {
+    Debug.Log(collision.gameObject);
+    if (collision.gameObject.CompareTag("Paddle"))
+    {
+      ballState = BallState.Active;
+    }
+  }
+
+  public void ServeBall()
+  {
+    rb.useGravity = true;
+    rb.velocity = Vector3.up * serveForce;
+    ballState = BallState.Waiting;
+  }
+
+  public void ResetBall(Vector3 position)
+  {
+    transform.position = position;
+    rb.velocity = Vector3.zero;
+    rb.angularVelocity = Vector3.zero;
+    rb.useGravity = false;
+    ballState = BallState.Held;
   }
 }
