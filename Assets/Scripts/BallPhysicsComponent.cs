7d3b8001ac96481296590b795943e2e95ba4f3b3
Shawn <shawnwerber@suu.edu>
2025-04-09 14:10:54 -0600

    Rename things based on check in

--- Assets/Scripts/BallPhysicsComponent.cs
+++ Assets/Scripts/BallPhysicsComponent.cs
@@ -7,83 +7,65 @@ public class BallPhysicsComponent : NetworkBehaviour
   public enum BallState { Held, Serving, Waiting, Active }
   public BallState ballState = BallState.Held;
   [SerializeField] private float _serveForce = 5f;
-  private Rigidbody _rb;
-
+  private Rigidbody _rigidbody;
 
   public override void OnNetworkSpawn()
   {
-    _rb = GetComponent<Rigidbody>();
-    _rb.useGravity = false;
+    _rigidbody = GetComponent<Rigidbody>();
+    _rigidbody.useGravity = false;
   }
 
   void Update()
   {
-    if (!IsOwner)
-    {
-      return;
-    }
-
+    if (!IsOwner) return;
+    UpdateBall();
+  }
+  void UpdateBall()
+  {
     switch (ballState)
     {
-      case BallState.Held:
-        _rb.velocity = Vector3.zero;
-        _rb.useGravity = false;
-
-        // if (controller button is pushed)
-        // {
-        //   ballState = BallState.Serving;
-        // }
-        break;
-
-      case BallState.Serving:
-        ServeBall();
-        break;
-
+      case BallState.Held: HoldBall(); break;
+      case BallState.Serving: ServeBall(); break;
       case BallState.Waiting:
         // insert check for user interaction here
         break;
-
-      case BallState.Active:
-        _rb.useGravity = true;
-        break;
+      case BallState.Active: _rigidbody.useGravity = true; break;
     }
+  }
+  void HoldBall()
+  {
+    _rigidbody.velocity = Vector3.zero;
+    _rigidbody.useGravity = false;
+
+    // if (controller button is pushed)
+    // {
+    //   ballState = BallState.Serving;
+    // }
   }
 
   void OnCollisionEnter(Collision collision)
   {
-    if (!IsOwner)
-    {
-      return;
-    }
+    if (!IsOwner) return;
 
     Debug.Log(collision.gameObject);
-    if (collision.gameObject.CompareTag("Paddle"))
-    {
-      ballState = BallState.Active;
-    }
+    if (collision.gameObject.CompareTag("Paddle")) ballState = BallState.Active;
   }
 
   public void ServeBall()
   {
-    if (!IsOwner)
-    {
-      return;
-    }
-    _rb.useGravity = true;
-    _rb.velocity = Vector3.up * _serveForce;
+    if (!IsOwner) return;
+    _rigidbody.useGravity = true;
+    _rigidbody.velocity = Vector3.up * _serveForce;
     ballState = BallState.Waiting;
   }
 
   public void ResetBall(Vector3 position)
   {
-    if (!IsOwner)
-    {
-      return;
-    }
+    if (!IsOwner) return;
     transform.position = position;
-    _rb.velocity = Vector3.zero;
-    _rb.angularVelocity = Vector3.zero;
-    _rb.useGravity = false;
+    _rigidbody.velocity = Vector3.zero;
+    _rigidbody.angularVelocity = Vector3.zero;
+    _rigidbody.useGravity = false;
     ballState = BallState.Held;
   }
 }
